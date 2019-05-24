f053cfb0cd4cb71059f69553373297ab54344aa1
Manos Markou <manos.markou@upstreamsystems.com>
2019-05-24 18:47:06 +0300

    aaaa

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import "./styles.css";
 
 const API_URL = "https://starwars.egghead.training/";
+const output = document.querySelector('#output');
+
+output.innerText = 'Loading...'
 
 const responsePromise = fetch(API_URL + "films");
 
 responsePromise.then(response => {
-  console.log(response);
+  return response.json()
+}).then( films => {
+  const filmTitles = films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map(film => `${film.episode_id} - ${film.title}`)
+    .join('\n')
+
+    output.innerText = filmTitles;
 });
